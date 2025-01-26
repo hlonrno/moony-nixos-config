a5bee83b0dceb97a874a8433a4ac004ea85bb4f1
hlonrno <moony25@proton.me>
2025-01-26 16:52:19 +0200

    catppuccin theme

--- moony/kitty/kitty.nix
+++ moony/kitty/kitty.nix
@@ -1,16 +1,14 @@
 {
   enable = true;
+  extraConfig = builtins.readFile ./Catppuccin-Mocha.conf;
   settings = {
-    cursor = "#cdd6f4"; # catppuccin Mocha/Text
-    cursor_shape = "block";
+    cursor_shape = "beam";
     cursor_shape_unfocused = "underline";
     cursor_blink_interval = -1;
-    cursor_trail = 1;
     scrollback_lines = 5000;
     scrollback_indicator_opacity = 0.5;
     touch_scroll_multiplier = 3.0;
     mouse_hide_wait = -1;
-    url_color = "#74c7ec"; # catppuccin Mocha/Sapphire
     url_style = "dotted";
     show_hyprerlink_targets = "yes";
     enable_terminal_bell = "no";
@@ -18,8 +16,5 @@
     active_border_color = "none";
     confirm_os_window_close = 0;
     background_opacity = 0.8;
-    # background_blur = 0.6;
-    background = "#313244"; # catppuccin Mocha/Surface0
-    foreground = "#1e1e2e"; # catppuccin Mocha/Base
   };
 }
