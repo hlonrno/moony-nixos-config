d835497f54a35884cd5ed3f5265285d277daa6c4
hlonrno <moony25@proton.me>
2025-01-26 17:53:50 +0200

    fixed hyprpaper monitor config

--- moony/hyprland/hyprpaper.nix
+++ moony/hyprland/hyprpaper.nix
@@ -4,6 +4,6 @@
     ipc = "off";
     splash = false;
     preload = [ "/etc/nixos/moony/hyprland/wallpaper.png" ];
-    wallpaper = [ "/etc/nixos/moony/hyprland/wallpaper.png" ];
+    wallpaper = [ "eDP-1,/etc/nixos/moony/hyprland/wallpaper.png" ];
   };
 }
