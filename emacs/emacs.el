bd881dfba30ec9cfab71d70de2cfb13791881cd2
hlonrno <moony25@proton.me>
2026-02-04 23:34:33 +0200

    Changed emacs config.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,6 +1,11 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
+(setq make-backup-files nil ; file~
+      auto-save-default nil ; #file#
+      create-lockfiles nil) ; .#file
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'visual)
 
 ;; Theme
 (load-theme 'catppuccin :no-confirm)
@@ -9,10 +14,10 @@
 
 ;;; Modes
 
-;; Company yas
+;; Company
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; Simple C mode (https://github.com/rexim/simpc-mode)
-(add-to-list 'load-path "/home/moony/.config/home-manager/emacs/simpc-mode.el")
-(require 'simpc-mode)
-(add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
+;; ;; Simple C (https://github.com/rexim/simpc-mode)
+;; (add-to-list 'load-path "/home/moony/.config/home-manager/emacs/simpc-mode.el")
+;; (require 'simpc-mode)
+;; (add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
